6a1ecbdf502c23e97926858f85d2ba82d39fe91b
ckle2101 <ckle2101@live.com>
2022-03-18 19:16:25 -0600

    Auto downloading lyrics
    Allows you to play a song while lyricFetcher is running and it will automatically request the page from Genius and download the lyrics.

--- lyricFetcher.py
+++ lyricFetcher.py
@@ -1,97 +1,135 @@
 from fileinput import filename
+from time import sleep
 import requests
 from bs4 import BeautifulSoup, element
 import os
-import sys
+from os.path import exists
+import re
+import mediaUpdater
 
-artist = sys.argv[1]
-song = sys.argv[2]
+while True:
 
-# artist = "ajr"
-# song = "weak"
+    sleep(1)
 
-song_hyphen = song.replace(" ","-")
-artist_hyphen = artist.replace(" ","-")
+    media_info = mediaUpdater.get_media_info()
 
-song_info = f"{artist_hyphen}-{song_hyphen}"
+    if media_info == None:
+        print("No media is playing.")
+        continue
 
-link = f"https://www.genius.com/{song_info}-lyrics"
+    # remove all characters that are not whitespace nor letters
+    artist = re.sub(r'[^\w\s]', '', media_info['artist'])
+    song = re.sub(r'[^\w\s]', '', media_info['title'])
 
-lyrics_array = []
-lyrics_string = ""
+    # make all double/triple/etc. spaces only one space
+    artist = re.sub(' +', ' ', artist)
+    song = re.sub(' +', ' ', song)
 
-response = requests.get(link)
-soup = BeautifulSoup(response.content, "html.parser")
+    # replace all spaces with hyphens
+    song_hyphen = song.replace(" ","-")
+    artist_hyphen = artist.replace(" ","-")
 
-lyrics = soup.find_all(class_="Lyrics__Container-sc-1ynbvzw-6 jYfhrf")
+    song_info = f"{artist_hyphen.lower()}-{song_hyphen.lower()}"
 
-for lyric in lyrics:
-    for linebreak in lyric:
-        linebreak_string = str(linebreak)
-        linebreak_first_two = linebreak_string[:2]
-        if linebreak_string[:2] == "<s":
-            continue
-        if linebreak_string[:2] == "<b":
-            if lyrics_array[-1] == "\n":
-                try:
-                    boolean = lyrics_array[-2] == "\n"
-                except:
-                    lyrics_array.append("\n")
-                else:
-                    if boolean:
+    if exists(f".\lyrics\{song_info}.txt"):
+        print("Lyrics are already downloaded.")
+        continue
+
+    link = f"https://www.genius.com/{song_info}-lyrics"
+
+    lyrics_array = []
+    lyrics_string = ""
+
+    print("Requesting lyrics...")
+
+    response = requests.get(link)
+
+    if str(response) == "<Response [404]>":
+        print("Lyrics were unable to be fetched. It is possible that the song name on Genius" +
+            " does not match the media's name, or maybe the current media does not have lyrics on Genius." +
+            " A file will still be made to function as a placeholder.")
+        # continue
+    else:
+        print("Lyrics successfully fetched.")
+
+
+    print("Downloading lyrics...")
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    lyrics = soup.find_all(class_="Lyrics__Container-sc-1ynbvzw-6 jYfhrf")
+
+    for lyric in lyrics:
+        for linebreak in lyric:
+            linebreak_string = str(linebreak)
+            linebreak_first_two = linebreak_string[:2]
+            if linebreak_string[:2] == "<s":
+                continue
+            if linebreak_string[:2] == "<b":
+                if lyrics_array[-1] == "\n":
+                    try:
+                        boolean = lyrics_array[-2] == "\n"
+                    except:
                         lyrics_array.append("\n")
-            else:
-                lyrics_array.append("\n")
-
-        elif linebreak_string[:2] == "<a":
-            linebreak_split = str(linebreak.find_all("span"))
-            linebreak_split = linebreak_split.split(">")
-            for item in linebreak_split:
-                item2 = item.split("<")[0]
-                if item2 not in {"[", "]", ""}:
-                    # print(item)
-                    lyrics_array.append(item2)
-                if item.endswith("<br/") and lyrics_array[-1] != "\n":
+                    else:
+                        if boolean:
+                            lyrics_array.append("\n")
+                else:
                     lyrics_array.append("\n")
-        else:
-            if type(linebreak) == element.Tag:
-                # print(linebreak.text)
-                linebreak_split = linebreak_string.split(">")
+
+            elif linebreak_string[:2] == "<a":
+                linebreak_split = str(linebreak.find_all("span"))
+                linebreak_split = linebreak_split.split(">")
                 for item in linebreak_split:
-                    item2 = item.split("<")[0]
+                    item2 = item.split("<", 1)[0]
                     if item2 not in {"[", "]", ""}:
                         # print(item)
                         lyrics_array.append(item2)
                     if item.endswith("<br/") and lyrics_array[-1] != "\n":
                         lyrics_array.append("\n")
-                # lyrics_array[-1] = lyrics_array[-1] + linebreak.text + "{"
-                continue
             else:
-                lyrics_array.append(linebreak)
+                if type(linebreak) == element.Tag:
+                    # print(linebreak.text)
+                    linebreak_split = linebreak_string.split(">")
+                    for item in linebreak_split:
+                        item2 = item.split("<", 1)[0]
+                        if item2 not in {"[", "]", ""}:
+                            # print(item)
+                            lyrics_array.append(item2)
+                        if item.endswith("<br/") and lyrics_array[-1] != "\n":
+                            lyrics_array.append("\n")
+                    # lyrics_array[-1] = lyrics_array[-1] + linebreak.text + "{"
+                    continue
+                else:
+                    lyrics_array.append(linebreak)
 
-lyrics_iter = iter(lyrics_array)
+    lyrics_iter = iter(lyrics_array)
 
-for item in lyrics_iter:
-    if item == "":
-        continue
-    else:
-        if item[-1] == "(":
-            item = item + next(lyrics_iter, "") + next(lyrics_iter, "")
-        if item[0] == "[":
-            item = "\n" + item
-        if item[-1] == "{":
-            item = item[:-1]
-            test = next(lyrics_iter, "")
-            item += test
+    for item in lyrics_iter:
+        if item == "":
+            continue
+        else:
+            if item[-1] == "(":
+                item = item + next(lyrics_iter, "") + next(lyrics_iter, "")
+            if item[0] == "[":
+                item = "\n" + item
+            if item[-1] == "{":
+                item = item[:-1]
+                test = next(lyrics_iter, "")
+                item += test
 
-    # item = item + "\n"
-    lyrics_string += item
+        # item = item + "\n"
+        lyrics_string += item
 
-print(lyrics_string)
+    # print(lyrics_string)
+    print("Lyrics downloaded.")
+    print("Saving lyrics as a .txt file...")
 
-filename = f".\{song_info}.txt"
+    filename = f".\lyrics\{song_info}.txt"
 
-os.makedirs(os.path.dirname(filename), exist_ok=True)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-with open(filename, "w", encoding="utf-8") as f:
-    f.write(lyrics_string)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(lyrics_string)
+    
+    print("Lyrics saved.")
---
+++ mediaUpdater.py
@@ -0,0 +1,24 @@
+# adapted from https://stackoverflow.com/a/66037406
+
+import asyncio
+from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
+
+async def get_media_info_helper():
+    sessions = await MediaManager.request_async()
+    current_session = sessions.get_current_session()
+    if current_session:  # there needs to be a media session running
+        info = await current_session.try_get_media_properties_async()
+
+        # song_attr[0] != '_' ignores system attributes
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+        # converts winrt vector to list
+        info_dict['genres'] = list(info_dict['genres'])
+
+        return info_dict
+    # else:
+    #     return 0
+
+def get_media_info():
+    return asyncio.run(get_media_info_helper())
+
